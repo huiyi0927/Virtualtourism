bceb0a36d8b8ec3a571ebc3ccd5e5a31bb3ed718
huiyi <1065389662@qq.com>
2022-11-26 19:52:51 +0800

    12

--- app/src/main/java/com/example/virtualtourism/MainActivity.java
+++ app/src/main/java/com/example/virtualtourism/MainActivity.java
@@ -2,13 +2,52 @@ package com.example.virtualtourism;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.location.Location;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 
 public class MainActivity extends AppCompatActivity {
+    TextView locationInfo;
+    LocationClient mLocationClient;
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        locationInfo = findViewById(R.id.locationInfo);
+        try {
+            mLocationClient = new LocationClient(getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        mLocationClient.registerLocationListener(new MyLocationListener());
+
+        requestLocation();
+
+
+    }
+
+    private void requestLocation() {
+        initLocation();
+        mLocationClient.start();
+    }
+
+    private void initLocation() {
+        LocationClientOption option = new LocationClientOption();
+    }
+
+
+    private class MyLocationListener extends BDAbstractLocationListener {
+
+        @Override
+        public void onReceiveLocation(BDLocation bdLocation) {
+
+        }
     }
 }
